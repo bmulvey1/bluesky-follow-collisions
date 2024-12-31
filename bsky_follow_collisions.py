8c63cc8d46fb29408d16ade9e6157e7c71dbdea4
Bea Mulvey <22732775+bmulvey1@users.noreply.github.com>
2024-12-30 22:42:49 -0500

    check if app password is used, determine which endpoint to use for non bsky PDSes

--- bsky_follow_collisions.py
+++ bsky_follow_collisions.py
@@ -1,23 +1,45 @@
-# this currently only works with bsky.social PDSes, going to tweak to support self hosted PDSes later
-
 import argparse
+import re
 import requests
 
 parser = argparse.ArgumentParser("bsky_follow_collisions")
-parser.add_argument("username", help="Bluesky username")
+parser.add_argument("handle", help="Bluesky handle")
 parser.add_argument("app_password", help="Bluesky app password")
 
 args = parser.parse_args()
 
-bsky_username = args.username
+bsky_handle = args.handle
 bsky_app_password = args.app_password
 
-session_url = "https://bsky.social/xrpc/com.atproto.server.createSession"
+app_password_regex = r"^[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}"
+match = re.match(app_password_regex, bsky_app_password)
+if match is None:
+    print("Use an app password instead of your actual password! Generate one at https://bsky.app/settings/app-passwords.")
+    exit(1)
+else:
+    pass
+
+
+bsky_resolve_handle_url = f"https://public.api.bsky.app/xrpc/com.atproto.identity.resolveHandle?handle=\
+{bsky_handle}"
+
+resolved_handle = requests.get(
+    bsky_resolve_handle_url, timeout=5).json()['did']
+
+# figure out what PDS to connect to
+
+plc_directory_url = f"https://plc.directory/{resolved_handle}"
+resp = requests.get(plc_directory_url, timeout=5).json()
+endpoint = resp['service'][0]['serviceEndpoint']
+
+print(f"using {endpoint} as PDS")
+
+session_url = f"{endpoint}/xrpc/com.atproto.server.createSession"
 
 headers = {"Content-Type": "application/json", "Accept": "application/json"}
 
 raw_data = '{"identifier": "%s", "password": "%s"}' % (
-    bsky_username, bsky_app_password)
+    bsky_handle, bsky_app_password)
 
 auth_req = requests.post(session_url, headers=headers,
                          data=raw_data, timeout=5)
@@ -25,10 +47,10 @@ bsky_token = auth_req.json()['accessJwt']
 
 headers = {"Authorization": f"Bearer {bsky_token}"}
 
-bsky_follows_url = f"https://bsky.social/xrpc/com.atproto.repo.listRecords?repo=\
-{bsky_username}&collection=app.bsky.graph.follow"
-bsky_filtered_follows_url = f"https://bsky.social/xrpc/app.bsky.graph.getFollows?actor=\
-{bsky_username}"
+bsky_follows_url = f"{endpoint}/xrpc/com.atproto.repo.listRecords?repo=\
+{bsky_handle}&collection=app.bsky.graph.follow"
+bsky_filtered_follows_url = f"{endpoint}/xrpc/app.bsky.graph.getFollows?actor=\
+{bsky_handle}"
 
 follow_dids = []
 cursor = ""
@@ -64,11 +86,11 @@ if presented_follow_count == len(follow_dids):
     print("follow count consistent, none are blocked or deleted")
     exit(0)
 else:
-    print("!!! follow count inconsistent !!!")
-
+    print("🚨🚨🚨 follow count inconsistent 🚨🚨🚨")
+    
 # now find out the subscribed blocklists
 
-bsky_listblocks_url = "https://bsky.social/xrpc/app.bsky.graph.getListBlocks"
+bsky_listblocks_url = f"{endpoint}/xrpc/app.bsky.graph.getListBlocks"
 
 listblocks = []
 cursor = ""
@@ -86,7 +108,8 @@ while 1:
 
 print(f"# subscribed blocklists: {len(listblocks)}")
 
-bsky_list_details_url = "https://bsky.social/xrpc/app.bsky.graph.getList"
+# no auth required
+bsky_list_details_url = f"{endpoint}/xrpc/app.bsky.graph.getList"
 
 collisions = []
 
